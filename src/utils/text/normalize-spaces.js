1844ddba26f5c0ac2bf80c2aa792431d7dac815a
2nthony <ant.hu@outlook.com>
2023-09-13 01:34:32 +0800

    fix: don't strip spaces
    it was opinion

--- src/utils/text/normalize-spaces.js
+++ src/utils/text/normalize-spaces.js
@@ -1,5 +1,7 @@
 const NORMALIZE_RE = /\s{2,}(?![^<>]*<\/(pre|code|textarea)>)/g;
 
 export default function normalizeSpaces(text) {
-  return text.replace(NORMALIZE_RE, ' ').trim();
+  return text;
+  // eslint-disable-next-line no-unreachable
+  return text.replace(NORMALIZE_RE, ' ');
 }
